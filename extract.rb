648ccc1eb197250aab6c4135deb4750154504bb5
Luca Pellicoro <luca.pellicoro@gmail.com>
2012-10-08 00:46:21 -0700

    Loop through each track to build a hash table then write to json file

--- extract.rb
+++ extract.rb
@@ -1,13 +1,33 @@
 #!/usr/bin/ruby
-require 'itunes/library'
+require 'rubygems'
+require 'digest/md5'
+
+require 'json' # gem install json_pure   
+require 'itunes/library' # gem install itunes-library
 
 library = 'small_library.xml'
+#library = 'big_library.xml'
 
 if not File.exist? library
 	puts "Warning! Could not find library file: " + library
 end
 
 library = ITunes::Library.load(library)
-puts library.playlists.map(&:name)
 
+tracks = Hash.new()
 
+for i in library.tracks do
+	# to_s avoids nil + string concatanation error
+	name = i.name.to_s
+	artist = i.artist.to_s
+	genre = i.genre.to_s
+
+	# Make an id to avoid track name conflicts
+	id = Digest::MD5.hexdigest(name + artist + genre)
+	
+	tracks[id] = Hash['name'=>name, 'artist'=>artist, 'genre'=>genre]
+end
+
+File.open("temp.json","w") do |f|
+  f.write(tracks.to_json)
+end
